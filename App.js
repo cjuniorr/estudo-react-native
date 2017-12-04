cc6575048759c70b1e8a1073eadfac1b7ab60513
Célio Junior <celiopaivajr@gmail.com>
2017-12-04 14:15:59 -0300

    Initial Commit

--- App.js
+++ App.js
@@ -5,53 +5,37 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const Estilos = {
+  principal: {
+    flex:1,
+    backgroundColor: 'cornflowerblue'
+  },
+  topo: {
+    flex:1,
+    backgroundColor: 'brown'
+  },
+  conteudo: {
+    flex:1,
+    backgroundColor: 'yellowgreen'
+  },
+  rodape: {
+    flex:1,
+    backgroundColor: 'orangered'
+  }
+};
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
+    const { principal, topo, conteudo, rodape } = Estilos;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={ principal }>
+        <Text style={ topo }>Topo</Text>
+        <Text style={ conteudo }>Conteúdo</Text>
+        <Text style={ rodape }>Rodape</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
