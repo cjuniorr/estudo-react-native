356a815026c3d5ccb485fa9e713038e43d9100e0
Célio Junior <celiopaivajr@gmail.com>
2017-12-04 11:11:12 -0300

    App finalizado

--- App.js
+++ App.js
@@ -6,52 +6,23 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const geraNumeroAleatorio = () => {
+  var numero_aleatorio = Math.floor(Math.random()*10);
+  alert(numero_aleatorio);
+}
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <View>
+        <Text>Gerador de números radômicos</Text>
+        <Button title="Gerar número randômico" onPress = {geraNumeroAleatorio}></Button>
+      </View> 
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
