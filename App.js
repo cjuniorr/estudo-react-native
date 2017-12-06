9134143d9172087bc0039e3c272795efd1927f61
Célio Junior <celiopaivajr@gmail.com>
2017-12-06 10:22:22 -0300

    Jogo finalizado

--- App.js
+++ App.js
@@ -10,31 +10,176 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Image
 } from 'react-native';
 
 export default class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario: '' }
+    this.state = { escolhaUsuario: '',
+                   escolhaComputador: '',
+                   resultado: '' }
   }
 
   jokenpo(escolhaUsuario){
-    this.setState({ escolhaUsuario : escolhaUsuario });
-     //alert(escolhaUsuario);
+    var numAleatorio = Math.floor(Math.random() * 3);
+    var escolhaComputador = '';
+    switch(numAleatorio){
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+
+    var resultado = '';
+
+    if(escolhaComputador == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Você ganhou'
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Você perdeu'
+      }
+    }
+
+    if(escolhaComputador == 'papel'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Você perdeu';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Empate'
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Você ganhou'
+      }
+    }
+
+    if(escolhaComputador == 'tesoura'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Você ganhou';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Você perdeu'
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Empate'
+      }
+    }
+
+    this.setState({ escolhaUsuario : escolhaUsuario,
+                    escolhaComputador: escolhaComputador,
+                    resultado: resultado
+                   });
   }
 
   render() {
     return (
       <View>
-        <Text>Escolha do computador</Text>
-        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado</Text>
-        <Button title="pedra" onPress={ () => { this.jokenpo('pedra') }}></Button>
-        <Button title="papel" onPress={ () => { this.jokenpo('papel') }}></Button>
-        <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura') }}></Button>
+        <Topo></Topo>
+
+        <View style={styles.painelAcoes}>
+
+          <View style={styles.btnEscolha}>
+            <Button title="pedra" onPress={ () => { this.jokenpo('pedra') }}></Button>
+          </View>
+
+          <View style={styles.btnEscolha}>
+            <Button title="papel" onPress={ () => { this.jokenpo('papel') }}></Button>
+          </View>
+
+          <View style={styles.btnEscolha}>
+            <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura') }}></Button>
+          </View>
+
+        </View>
+
+        <View style={styles.palco}>
+          <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+          
+          <Icone escolha={this.state.escolhaComputador} jogador='Computador'></Icone>
+          <Icone escolha={this.state.escolhaUsuario} jogador='Você'></Icone>
+
+        </View>
+
+
+       </View>
+    );
+  }
+}
+
+class Topo extends Component {
+  render(){
+    return(
+      <View>
+        <Image source={require('./img/jokenpo.png')}/>
       </View>
     );
   }
 }
+
+class Icone extends Component {
+  render(){
+    if(this.props.escolha == 'pedra'){
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+          <Image source={require('./img/pedra.png')}/>
+        </View>
+      );
+    } else if (this.props.escolha == 'papel') {
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+          <Image source={require('./img/papel.png')}/>
+        </View>
+      );
+    } else if (this.props.escolha == 'tesoura') {
+      return (
+        <View style={styles.icone}>
+          <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+          <Image source={require('./img/tesoura.png')}/>
+        </View>
+      );
+    } else {
+      return false;
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  btnEscolha: {
+    width: 90
+  },
+  painelAcoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  palco: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  txtResultado: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 60
+  },
+  icone: {
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  txtJogador: {
+    fontSize: 18
+  }
+})
